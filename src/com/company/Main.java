742a60426903bab1a762cd491abd721b6e3120b6
Mikita Valatovich <mr.volotovich@gmail.com>
2021-04-01 19:30:46 +0200

    Skeleton

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        jsonInitialization();
+
+        if(args.equals("title")){
+            findTitle();
+        }
+
+        exportToJSON();
+    }
+
+    private static void jsonInitialization(){ } //Marek ...
+
+
+    private static void findTitle(){ } //Marek
+
+    private static void findVersions(){ } //Mikita
+
+    private static void findBibliography(){ } //Kunal
+
+    private static void findTableOfContents(){ }
+
+    private static void findRevisions(){ }
+
+    private static void findOther(){ }
+
+    private static void exportToJSON(){ }
+}
