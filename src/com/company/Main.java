c6364cb81004cd844ed44b22953f5fac867a99c8
Mikita Valatovich <mr.volotovich@gmail.com>
2021-04-04 16:05:48 +0200

    findVersions - done findTableOfContents - done partly importPdfFile - done countLines - done (for tables) __ else directory with our pdfs and examples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,67 +1,152 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
-public class Main {
-    static File inputFile = null;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public static void main(String[] args) {
 //      Expected input:
 //      path title versions other
 //      path all
-        try{
-            System.out.println("Path is:" + args[0]);
-            importTxtFile(args[0]) ;
 
-        } catch (Exception e) {
-            System.out.println("Input error");
-            e.printStackTrace();
-        }
+public class Main {
+    static File inputFile = null;
+    static int numberOfLines = 0;
 
+    public static void main(String[] args) throws Exception {
+        importPdfFile(args[0]);
         jsonInitialization();
+        numberOfLines = countLines(inputFile);
 
-        if(args[1].contains("version")){ //.matches detects separate word
+        if (args[1].contains("version")) { //.matches detects separate word
             System.out.println("version detected");
             findVersions();
+        }
+
+        if (args[1].contains("table")) { //.matches detects separate word
+            System.out.println("table detected");
+            findTableOfContents();
+        }
+
+        if (args[1].contains("all")) { //.matches detects separate word
+            System.out.println("all detected");
+            findVersions();
+            findTableOfContents();
+            findTitle();
+            findBibliography();
+            findOther();
+            findRevisions();
         }
 
         exportToJSON();
     }
 
-    private static void jsonInitialization(){ } //Marek ...
+    private static void jsonInitialization() {
+    } //Marek ...
 
-    private static void findTitle(){ } //Marek
+    private static void findTitle() {
+    } //Marek
 
-    private static void findVersions(){
+    private static void findVersions(){ // Mikita
+        Map<String, String> patternMap = new HashMap<>();
+        patternMap.put("eal", "EAL+\\s?+[0-9]+\\+?");
+        patternMap.put("global_platform", "GlobalPlatform\\s?\\d\\.\\d\\.\\d");
+        patternMap.put("java_card", "Java\\s?Card\\s?(Platform)?\\s?v?\\d\\.?\\d?\\.?\\.?\\d?");
+        patternMap.put("sha", "(SHA|sha)-\\d++");
+        patternMap.put("rsa", "(RSA|rsa)[\\s|-]\\d\\d\\d\\d?");
+        patternMap.put("ecc", "ECC[\\s|-]\\d\\d\\d");
+        patternMap.put("des", "[0-9]-?DES");
 
-    } //Mikita
-
-    private static void findBibliography(){ } //Kunal
-
-    private static void findTableOfContents(){ }
-
-    private static void findRevisions(){ }
-
-    private static void findOther(){ }
-
-    private static void exportToJSON(){ }
-
-    private static void importTxtFile(String path){
-        try {
-            inputFile = new File(path);
-
-            Scanner myReader = new Scanner(inputFile); //file output for example
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+        patternMap.forEach((key, value) -> {
+            ArrayList<String> matches = new ArrayList<>();
+            Pattern pattern = Pattern.compile(value);
+            System.out.println(key + ":");
+            Iterator<String> lineIterator = null;
+            try {
+                lineIterator = Files.lines(Paths.get(inputFile.getPath())).iterator();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            myReader.close();
+            while (true) {
+                assert lineIterator != null;
+                if (!lineIterator.hasNext()) break;
+                String line = lineIterator.next();
+                Matcher matcher = pattern.matcher(line);
+                while(matcher.find()){
+                    if(!matches.contains(matcher.group())){
+                        matches.add(matcher.group());
+                    }
+                }
+            }
+            matches.forEach(System.out::println);
+        });
+    }
 
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-            e.printStackTrace();
+    private static void findBibliography() { //Kunal
+    }
+
+    private static void findTableOfContents() throws Exception { // Mikita
+        //TODO
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile)));
+        for(int i = 1; i < numberOfLines+1; ++i){
+            String currentLine = reader.readLine();
+            Pattern pattern = Pattern.compile("(Contents)|(CONTENTS)|(INDEX)");
+            Matcher matcher = pattern.matcher(currentLine);
+            while(matcher.find()){
+                System.out.println("Line number: " + i);
+                System.out.println(currentLine);
+            }
+        }
+
+    }
+
+    private static void findRevisions() {
+    }
+
+    private static void findOther() {
+    }
+
+    private static void exportToJSON() {
+    }
+
+    private static void importPdfFile(String path) throws IOException { //Mikita
+        inputFile = new File(path);
+        PDDocument doc = PDDocument.load(inputFile);
+        PDFTextStripper stripper = new PDFTextStripper();
+
+        File convertedToTxtFile = new File("./temp.txt");
+        FileWriter writer = new FileWriter(convertedToTxtFile);
+        writer.write(stripper.getText(doc));
+        writer.flush();
+
+        inputFile = convertedToTxtFile;
+    }
+
+    public static int countLines(File file) throws IOException { //Mikita
+        try (InputStream is = new BufferedInputStream(new FileInputStream(file))) {
+            byte[] c = new byte[1024];
+            int readChars = is.read(c);
+            if (readChars == -1)
+                return 0;
+            int count = 0;
+            while (readChars == 1024) {
+                for (int i = 0; i < 1024; )
+                    if (c[i++] == '\n')
+                        ++count;
+                readChars = is.read(c);
+            }
+            while (readChars != -1) {
+                for (int i = 0; i < readChars; ++i)
+                    if (c[i] == '\n')
+                        ++count;
+                readChars = is.read(c);
+            }
+            return count == 0 ? 1 : count;
         }
     }
 }
